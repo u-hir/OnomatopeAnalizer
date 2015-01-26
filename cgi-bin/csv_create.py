b18e04df00eaa01d35651b0df6b3090586d2a1ae
hiroki uema <hirokiuema@Hiroki-Uema-no-MBA.local>
2015-01-26 14:06:14 +0900

    csv書き出しok

--- cgi-bin/csv_create.py
+++ cgi-bin/csv_create.py
@@ -12,10 +12,12 @@ checklist = form.getlist('check')
 
 onomatope = form['onomatope_ppp'].value
 
-csvfile = open(onomatope + ".csv", "w")
+csvfile = open("cgi-bin/RefiningOutputs/"+ onomatope + ".csv", "w")
 
 print "Content-Type:text/html; charset:utf-8; \n";
 print '<p>' + onomatope + '</p>'
 for item in checklist:
     print item + "</br>"
-
+    csvfile.write(item + "\n")
+    print "ok"
+#csvfile.close()
