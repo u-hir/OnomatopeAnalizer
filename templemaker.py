9e6b674bd54835aa3279932776fda43cfec48ea8
hiroki uema <hirokiuema@Hiroki-Uema-no-MBA.local>
2015-01-28 08:43:24 +0900

    templemaker.py完成

--- templemaker.py
+++ templemaker.py
@@ -9,12 +9,13 @@ sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 def main():
     # 読み込むファイルのパスを取得
-#    sentence_paths = glob.glob('sentences/*.txt')
+    sentence_paths = glob.glob('sentences/*.txt')
 #    sentence_paths = ["sentences/ざあざあ.txt","sentences/しとしと.txt"]
-    sentence_paths = ["ざあざあ.txt","しとしと.txt"]
+#    sentence_paths = ["ざあざあ.txt","しとしと.txt"]
     for sentence_path in sentence_paths:
-        print "---------------------------------------"
+#        print "---------------------------------------"
         filename = get_filename(sentence_path)
+        filename = normalize_unicode_nfc(filename)
         sentence_file = open(sentence_path, 'r')
         lines = sentence_file.readlines()
         word_dics = get_word_dics(lines, filename)
@@ -26,7 +27,7 @@ def get_word_dics(lines, filename):
     m = MeCab.Tagger('-Ochasen -u userdic.dic')
     #前文名詞用リスト
     frontnouns = []
-    nouns_freq_dic = {}
+    combi_freq_dic = {}
     #後文動詞用dic
     rearact = {}
     act_freq_dic = {}
@@ -41,28 +42,41 @@ def get_word_dics(lines, filename):
         while front:
             frontfeatures = front.feature.split(',')
             if frontfeatures[0] == "助詞":
-                if frontfeatures[6] == "が":
+                if frontfeatures[6] == "が" or frontfeatures[6] == "を" or frontfeatures[6] == "に" :
                     frontterm = preterm
             preterm = frontfeatures[6]
             front = front.next
 
         # オノマトペの直後にある動詞
+        if len(linesplited) == 1:
+            break
         rear = m.parseToNode(linesplited[1])
         if frontterm == str():
-            break
+            pass
         else:
             while rear:
                 rearfeatures = rear.feature.split(',')
                 if rearfeatures[0] == "動詞":
-                    print frontterm.decode('utf-8') + "," + rearfeatures[6].decode('utf-8') + "," + line.decode("utf-8")
-                    frontnouns.append(frontterm.decode('utf-8') + "," + rearfeatures[6].decode('utf-8') + "," + line.decode("utf-8"))
+#                    print frontterm.decode('utf-8') + "," + rearfeatures[6].decode('utf-8') + "," + line.decode("utf-8")
+                    frontnouns.append(frontterm.decode('utf-8') + "/" + rearfeatures[6].decode('utf-8'))
                     break
                 else:
                     rear = rear.next
-    for f in frontnouns:
-        print "ok"
-        print f
-        print "ok"
+
+    #単語組み合わせの回数カウント
+    counter = Counter(frontnouns)
+    for word, cnt in counter.most_common():
+        combi_freq_dic.setdefault(word, cnt)
+    combi_freq_list = combi_freq_dic.items()
+
+    #出力
+    write_path = "templates/"+filename+".csv"
+    reset_file(write_path)
+    write_file = open(write_path, 'a')
+    for k, v in sorted(combi_freq_dic.items(),key=lambda x : x[1],reverse=True):
+        write_line = k + "," + str(v)
+        write_file.write(write_line.encode('utf-8')+'\n')
+    print filename.decode('utf-8')
 
     # for k, v in nouns_count_dic.items():
     #     if int(v) != 1:
@@ -113,6 +127,13 @@ def normalize_unicode_nfc(word):
     word_unicode_nfc = unicodedata.normalize("NFC", word_unicode_nfd) # unicode(NFD)からunicode(NFC)に変換
     word_utf8 = word_unicode_nfc.encode('utf-8') # unicode to utf-8(str)
     return word_utf8
+
+def normalize_unicode_nfc(word):
+    word_unicode_nfd = word.decode('utf-8') # strからunicode(NFD)に変換
+    word_unicode_nfc = unicodedata.normalize("NFC", word_unicode_nfd) # unicode(NFD)からunicode(NFC)に変換
+    word_utf8 = word_unicode_nfc.encode('utf-8') # unicode to utf-8(str)
+    return word_utf8
+
 
 
 #メイン。指定されたファイルをdef mainにかけて、結果をprintする。
